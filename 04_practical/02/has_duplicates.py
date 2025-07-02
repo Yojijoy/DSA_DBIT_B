6cfe9e8468b0828faf314383e663152e2e7bb93c
Yojijoy <nyawira.wanjohi@strathmore.edu>
2025-07-02 16:12:49 +0300

    Add files via upload
    Admission No.193569 Joy Wanjohi

---
+++ 04_practical/02/has_duplicates.py
@@ -0,0 +1,7 @@
+def has_duplicates(lst):
+    for i in range(len(lst)):
+        for j in range(i + 1, len(lst)):
+            if lst[i] == lst[j]:
+                return True
+    return False
+   
